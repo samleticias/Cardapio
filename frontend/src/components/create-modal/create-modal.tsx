5e8d832d23dd01414abf7c65c311afebf80742e7
samleticias <sammyaleticia09@gmail.com>
2024-09-30 22:45:50 -0300

    feat: cadastra novos itens do cardápio

---
+++ frontend/src/components/create-modal/create-modal.tsx
@@ -0,0 +1,61 @@
+import { useEffect, useState } from 'react';
+import { useFoodDataMutate } from '../../hooks/useFoodDataMutate.ts';
+import { FoodData } from '../../interface/FoodData.ts';
+
+import "./modal.css";
+
+interface InputProps {
+    label: string,
+    value: string | number,
+    updateValue(value: any): void
+}   
+
+interface ModalProps {
+    closeModal(): void
+}
+
+const Input = ({ label, value, updateValue }: InputProps) => {
+    return (
+        <>
+            <label>{label}</label>
+            <input value={value} onChange={event => updateValue(event.target.value)}></input>
+        </>
+    )
+}
+
+export function CreateModal({ closeModal }: ModalProps){
+    const [title, setTitle] = useState("");
+    const [price, setPrice] = useState(0);
+    const [image, setImage] = useState("");
+    const { mutate, isSuccess, isPending } = useFoodDataMutate();
+
+    const submit = () => {
+        const foodData: FoodData = {
+            title, 
+            price,
+            image
+        }
+        mutate(foodData)
+    }
+
+    useEffect(() => {
+        if(!isSuccess) return 
+        closeModal();
+    }, [isSuccess])
+
+    return(
+        <div className="modal-overlay">
+            <div className="modal-body">
+                <h2>Cadastre um Novo Item no Cardápio:</h2>
+                <form className="input-container">
+                    <Input label="Título" value={title} updateValue={setTitle}/>
+                    <Input label="Preço R$" value={price} updateValue={setPrice}/>
+                    <Input label="Imagem" value={image} updateValue={setImage}/>
+                </form>
+                <button onClick={submit} className="btn-secondary">
+                    {isPending ? 'Postando...' : 'Postar'}
+                </button>
+            </div>
+        </div>
+    )
+}
